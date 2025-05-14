927c3bd919d00279daf37f6bf1d9ccab7657135d
karimbobboi <abdulkarimbobboi@gmail.com>
2025-05-14 19:46:16 +0100

    add tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "*",
+    "./projects/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
