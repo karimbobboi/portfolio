2d696d1756c78853694af0aeca819275abd975b9
karimbobboi <abdulkarimbobboi@gmail.com>
2025-05-15 14:15:30 +0100

    remove production check in next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // output: 'export',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
