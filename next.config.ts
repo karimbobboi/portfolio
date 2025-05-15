732f520a7d434488658a9ab04a96bb2295dd7aae
karimbobboi <abdulkarimbobboi@gmail.com>
2025-05-15 16:36:41 +0100

    update publish workflow to trigger on main branch and adjust next.config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const isGithubPages = process.env.NODE_ENV === 'production';
 const repo = 'portfolio';
 
 const nextConfig: NextConfig = {
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
   output: "export",
   images: {
     unoptimized: true,
