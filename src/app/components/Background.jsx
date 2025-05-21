df06b00014ed1e2dcab5d998a0b89617c9626ff4
karimbobboi <abdulkarimbobboi@gmail.com>
2025-05-21 17:21:42 +0100

    fix background re render

--- src/app/components/Background.jsx
+++ src/app/components/Background.jsx
@@ -8,12 +8,14 @@ const Background = memo(function Background() {
   const gradientRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
 
+  let gradientInitialized = false;
+
   useEffect(() => {
     console.log("here")
-    if (!gradientRef.current) {
+    if (!gradientRef.current && !gradientInitialized) {
       gradientRef.current = new Gradient();
       gradientRef.current.initGradient("#gradient-canvas");
-      
+      gradientInitialized = true;
       // add delay before showing gradient
       setTimeout(() => {
         setIsVisible(true);
